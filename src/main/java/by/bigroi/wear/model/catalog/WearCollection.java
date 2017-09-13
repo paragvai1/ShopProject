e9d2b159574de45d3d512f99d84968e392f36e4c
AndreyMushkovski <andrey@amberlogistic.com>
2017-09-13 10:17:59 +0300

    таблица коллекции

--- src/main/java/by/bigroi/wear/model/catalog/WearCollection.java
+++ src/main/java/by/bigroi/wear/model/catalog/WearCollection.java
@@ -1,4 +1,41 @@
 package by.bigroi.wear.model.catalog;
 
+import by.bigroi.wear.model.product.Product;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table (name = "COLLECTION")
+
 public class WearCollection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_COLLECTION")
+    private long id;
+
+    @Column (name = "NAME", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "COLLECTION", cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
--- src/main/java/by/bigroi/wear/model/product/Product.java
+++ src/main/java/by/bigroi/wear/model/product/Product.java
@@ -1,4 +1,72 @@
 package by.bigroi.wear.model.product;
 
+import by.bigroi.wear.model.catalog.WearCollection;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PRODUCT")
+
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_Product")
+    private long id;
+
+    @Column (name = "NAME", nullable = false)
+    private String name;
+
+    @Column (name = "DESCRIPTION", nullable = false)
+    private String description;
+
+    @Column (name = "PICTURE", nullable = false)
+    private Object picture;
+
+    @Column (name = "PRICE", nullable = false)
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_COLLECTION")
+    private WearCollection wearCollection;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Object getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Object picture) {
+        this.picture = picture;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public WearCollection getWearCollection() {
+        return wearCollection;
+    }
+
+    public void setWearCollection(WearCollection wearCollection) {
+        this.wearCollection = wearCollection;
+    }
 }
